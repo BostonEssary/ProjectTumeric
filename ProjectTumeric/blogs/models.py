fe5a8c2b675fba478a62a9665ed75b56c64b5614
Boston Essary <bostoncessary@gmail.com>
2023-04-05 12:38:42 -0700

    blogs models brevity additions

--- ProjectTumeric/blogs/models.py
+++ ProjectTumeric/blogs/models.py
@@ -4,6 +4,12 @@ class Title(models.Model):
     title_text = models.CharField(max_length=20)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.title_text
+
 class Body(models.Model):
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
     body_text = models.TextField()
+
+    def __str__(self):
+        return self.body_text
--- ProjectTumeric/users/models.py
+++ ProjectTumeric/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class UserName(models.Model):
+    username = models.CharField(max_length=20)
 
+class Password(models.Model):
+    password = models.CharField()
