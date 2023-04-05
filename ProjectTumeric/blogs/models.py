9775c4c3b6d3b3fc47fbde79c9f94f1a9939782a
Boston Essary <bostoncessary@gmail.com>
2023-04-05 12:02:13 -0700

    boiler for users, reviews and items

--- ProjectTumeric/blogs/models.py
+++ ProjectTumeric/blogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Title(models.Model):
+    title_text = models.CharField(max_length=20)
+    pub_date = models.DateTimeField("date published")
+
+class Body(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    body_text = models.TextField()
